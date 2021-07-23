2d2697403a7ef2e233ff419691285b8379725d68
MarritNL <86615967+MarritNL@users.noreply.github.com>
2021-07-23 15:40:05 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+const myAge = 19;
+
+if (myAge < 18) {
+    document.write ("You are too young, you can't enter. ");
+} else if (myAge == 18) {
+    document.write ("You can enter. ");
+} else {
+     document.write ("You can enter. ");
+} 
+
+const isFemale = false
+
+if (isFemale) {
+    document.write ("Would you like to buy some tickets?")
+}
+else { 
+    document.write ("I am sorry but you can't buy tickets, we are really biased here.")
+}
+
+const driverStatus = "henk"
+
+if (driverStatus == "Bob") {
+    document.write ("You can drive. ");
+} 
+else{
+    document.write ("You can't drive. Call a taxi. ");
+} 
